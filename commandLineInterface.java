7262235fe77375211c252be383068af1531818e2
Zi-Gao <gaogaohahaa@gmail.com>
2021-11-14 11:05:35 +0800

    feat:Add input control

--- commandLineInterface.java
+++ commandLineInterface.java
@@ -1,8 +1,7 @@
-import java.util.Scanner;
 /*
  * @Author: Zi_Gao
  * @Date: 2021-11-13 20:24:47
- * @LastEditTime: 2021-11-13 21:14:47
+ * @LastEditTime: 2021-11-14 11:03:03
  * @LastEditors: Zi_Gao
  * @Description: command-line interface
  * @FilePath: /MathTools/commandLineInterface.java
@@ -10,7 +9,6 @@ import java.util.Scanner;
  */
 public class commandLineInterface {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
         int choice;
         while(true) {
             System.out.println("-----------Tool list-----------");
@@ -18,17 +16,16 @@ public class commandLineInterface {
             System.out.println("|2.exit                       |");
             System.out.println("-------------------------------");
             System.out.print("Please input your choice: ");
-            choice = sc.nextInt();
+            choice = input.inputInt();
             if(choice == 1){
                 greatestCommonDivisorCLI.main(args);
             }
             else if(choice == 2){
                 System.out.println("Bye!");
-                sc.close();
                 break;
             }
             else{
-                System.out.println("Invalid input!");
+                System.out.println("Invalid input! Please input again:");
             }
         }
     }
--- greatestCommonDivisorCLI.java
+++ greatestCommonDivisorCLI.java
@@ -1,9 +1,7 @@
-import java.util.Scanner;
-
 /*
  * @Author: Zi_Gao
  * @Date: 2021-11-13 20:37:20
- * @LastEditTime: 2021-11-13 21:14:55
+ * @LastEditTime: 2021-11-14 11:02:19
  * @LastEditors: Zi_Gao
  * @Description: command-line interface
  * @FilePath: /MathTools/greatestCommonDivisorCLI.java
@@ -11,19 +9,12 @@ import java.util.Scanner;
  */
 public class greatestCommonDivisorCLI {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
         while(true){
-            System.out.println("Please input the number, input 0 to start the calculation:");
-            long[] in = new long[1000];
-            for (int i = 0;i < 1000;i++) {
-                in[i] = sc.nextLong();
-                if (in[i] == 0) {
-                    break;
-                }
-            }
+            System.out.println("Please input the numbers, separated by spaces, and enter to start the calculation:");
+            long[] in = input.inputLongArray();
             System.out.println("The greatest common divisor is:" + greatestCommonDivisor.gcd(in));
             System.out.print("Do you exit?[Y/n]:");
-            String strIn = sc.next();
+            String strIn = input.inputString();
             if (strIn.equals("n") || strIn.equals("N")) {
                 continue;
             } else {
